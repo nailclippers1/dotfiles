c924f0528deeb095d0b90b6310366c7caca2a6b3
nailclippers <taishi.shinzaki@gmail.com>
2022-04-22 11:37:03 +0900

    2022/04/22 11:37:03

--- PowerShell/Microsoft.PowerShell_profile.ps1
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -10,6 +10,9 @@ Set-Alias nvim 'C:\Program Files\Neovim\bin\nvim.exe'
 Set-Alias vim 'C:\Program Files\Neovim\bin\nvim.exe'
 Set-Alias vi 'C:\Program Files\Neovim\bin\nvim.exe'
 
+Set-Alias gitup 'git add -A && git commit -m $(Get-Date -F G) && git push'
+#Set-Alias gitup 'git add -A && git commit -m $(Get-Date -F G) && git push origin $(git branch | sed -n -e "s/^\* \(.*\)/\1/p")'
+
 #Commands
 #ll
 function CustomListChildItems { Get-ChildItem $args[0] -force | Sort-Object -Property @{ Expression = 'LastWriteTime'; Descending = $true }, @{ Expression = 'Name'; Ascending = $true } | Format-Table -AutoSize -Property Mode, Length, LastWriteTime, Name }
