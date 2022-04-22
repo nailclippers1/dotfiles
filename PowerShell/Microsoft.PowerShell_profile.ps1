4db77363692a1516f0252179fdf5e6504175fd76
nailclippers <taishi.shinzaki@gmail.com>
2022-04-22 10:51:54 +0900

    2022/04/22

---
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,18 @@
+# edit the following key to change $profile path (Microsoft.PowerShell_profile.ps1) 
+#HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders
+
+# allow PowerShell scripts to run
+if((Get-ExecutionPolicy -Scope LocalMachine) -ne "RemoteSigned"){Set-ExecutionPolicy RemoteSigned -Scope CurrentUser -Force}
+
+#Aliases
+Set-Alias grep Select-String
+Set-Alias nvim 'C:\Program Files\Neovim\bin\nvim.exe'
+Set-Alias vim 'C:\Program Files\Neovim\bin\nvim.exe'
+Set-Alias vi 'C:\Program Files\Neovim\bin\nvim.exe'
+
+#Commands
+#ll
+function CustomListChildItems { Get-ChildItem $args[0] -force | Sort-Object -Property @{ Expression = 'LastWriteTime'; Descending = $true }, @{ Expression = 'Name'; Ascending = $true } | Format-Table -AutoSize -Property Mode, Length, LastWriteTime, Name }
+sal ll CustomListChildItems
+
+
